961c68959e4bfbab7b29b2e692042c5dd0b1dd23
Phan Minh Dương <phanminhduong@hotmail.com>
2019-01-17 00:52:17 +0700

    init project

---
+++ public/web/precache-manifest.04e9464ff1ffe6d06ace50fdd49ecd60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "127ebf2ab585ab24ea61",
+    "url": "/static/js/main.127ebf2a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "127ebf2ab585ab24ea61",
+    "url": "/static/css/main.d3eb4448.chunk.css"
+  },
+  {
+    "revision": "518cc0cfebce3e2071b7b337372cea74",
+    "url": "/index.html"
+  }
+];
